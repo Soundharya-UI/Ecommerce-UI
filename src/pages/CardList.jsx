22d7839ef00ca33fbb411c48eab17efcb4f1b092
soundharya-UI <soundharya.s2611@gmail.com>
2025-02-27 13:13:23 +0530

    image changes

--- src/pages/CardList.jsx
+++ src/pages/CardList.jsx
@@ -7,61 +7,61 @@ export const CardList = () => {
 const products = [
     {
         "id":1,
-        "image": "/assets/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
+        "image": "/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":2,
-        "image": "/assets/images/icons8-team-7LNatQYMzm4-unsplash.jpg",
+        "image": "/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":3,
-        "image": "/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+        "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":4,
-        "image": "/assets/images/milena-trifonova-pHqt1DsHCx0-unsplash.jpg",
+        "image": "/images/milena-trifonova-pHqt1DsHCx0-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":5,
-        "image": "/assets/images/tomasz-gawlowski-YDZPdqv3FcA-unsplash.jpg",
+        "image": "/images/tomasz-gawlowski-YDZPdqv3FcA-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":6,
-        "image": "/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+        "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":7,
-        "image": "/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+        "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":8,
-        "image": "/assets/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
+        "image": "/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":9,
-        "image": "/assets/images/icons8-team-7LNatQYMzm4-unsplash.jpg",
+        "image": "/images/icons8-team-7LNatQYMzm4-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     },
     {
         "id":10,
-        "image": "/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+        "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
         "name": "Boat Rockerz 450",
         "price": 149
     }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,55 +6,55 @@ export const Cart = () => {
     const products = [
         {
             "id":1,
-            "image": "src/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+            "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":2,
-            "image": "/src/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+            "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":3,
-            "image": "/src/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+            "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":4,
-            "image": "/assets/images/milena-trifonova-pHqt1DsHCx0-unsplash.jpg",
+            "image": "/images/milena-trifonova-pHqt1DsHCx0-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":5,
-            "image": "../assets/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
+            "image": "/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":6,
-            "image": "../assets/images/ervo-rocks-Zam8TvEgN5o-unsplash.png",
+            "image": "/images/ervo-rocks-Zam8TvEgN5o-unsplash.png",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":7,
-            "image": "/assets/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
+            "image": "/images/lee-campbell-GI6L2pkiZgQ-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":8,
-            "image": "/assets/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
+            "image": "/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         },
         {
             "id":9,
-            "image": "/assets/images/icons8-team-7LNatQYMzm4-unsplash.jpg",
+            "image": "/images/icons8-team-7LNatQYMzm4-unsplash.jpg",
             "name": "Boat Rockerz 450",
             "price": 149
         }
